8bb710f8dde30d434a946124542a4e18fe146b96
Minkyu0424 <yng1404@gmail.com>
2025-01-18 14:11:02 +0900

    🎉  [init] : 프로젝트 초기세팅

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,3 +6,4 @@ export default defineConfig({
     baseUrl: 'http://localhost:4173',
   },
 })
+
--- eslint.config.js
+++ eslint.config.js
@@ -17,12 +17,12 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
@@ -32,3 +32,4 @@ export default [
   },
   skipFormatting,
 ]
+
