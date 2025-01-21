26b8e9eb2fee6226a2dc9db221e8fa5f4be4d6c0
Minkyu0424 <yng1404@gmail.com>
2025-01-21 11:33:14 +0900

    :wrench: [init] : tailwind 재적용

---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary1: '#7879EB',
+        primary2: '#F7F9FF',
+        black: '#18181B',
+        body: '#71717A',
+        disabled: '#A1A1AA',
+        border: {
+          1: '#D4D4D8',
+          2: '#E4E4E7'
+        },
+        background: {
+          1: '#E4E4E7',
+          2: '#F4F4F5'
+        },
+        red: {
+          1: '#DC2626',
+          2: '#FEF2F2'
+        },
+        orange: {
+          1: '#FB923C',
+          2: '#FFF7ED'
+        },
+        yellow: {
+          1: '#FACC15',
+          2: '#FEFCE8'
+        },
+        green: {
+          1: '#22C55E',
+          2: '#F0FDF4'
+        },
+        blue: {
+          1: '#3B82F6',
+          2: '#EFF6FF'
+        },
+        indigo: {
+          1: '#6366F1',
+          2: '#EEF2FF'
+        },
+        violet: {
+          1: '#8B5CF6',
+          2: '#F5F3FF'
+        },
+        gray: {
+          1: '#A1A1AA',
+          2: '#F4F4F5'
+        }
+      }
+    }
+  },
+  plugins: []
+}
