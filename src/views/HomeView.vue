eaf26abf85b28b4cb2ff32ef94235baa94980110
jiyeon <bjyy0201@gmail.com>
2025-01-22 13:43:46 +0900

    🔥 [remove] : 기존 불필요 파일 제거

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import TestWorld from '@/components/TestWorld.vue'
-</script>
-
-<template>
-  <main class="w-full h-screen">
-    <TestWorld msg="개발 시작 자유롭게 파일구조를 변경해주세요" />
-  </main>
-</template>
