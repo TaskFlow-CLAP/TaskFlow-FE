8cbdfdc7ee1b402b6ba327e1d6373db357babaa4
jiyeon <bjyy0201@gmail.com>
2025-01-22 15:57:51 +0900

    ✨ [feat] : 로그인 여부 localStorage로 호출

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,10 +27,10 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const isLogined = false // 로그인 상태 서버로부터 받아와야함
+  const isLogined = localStorage.getItem('isLoggedIn') // 로그인 상태 서버로부터 받아와야함, 우선 세션스토리지로 설정
   console.log('isLogined:', isLogined)
 
-  if (to.path === '/pw-change' && !isLogined) {
+  if (to.path === '/pw-change' && (isLogined == 'false' || isLogined == null)) {
     alert('로그인이 필요합니다.')
     next('/login') // 로그인 페이지로 리다이렉트
   } else {
