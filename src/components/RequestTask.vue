3dcffcf990c6a8c478cccaa48d93a1457cc21b7b
Minkyu0424 <yng1404@gmail.com>
2025-01-22 21:07:50 +0900

    :lipstick: [design] : 작업요청 공통 드롭다운 UI 제작

---
+++ src/components/RequestTask.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="w-[600px] flex flex-col px-6 gap-y-6">
+    <div class="w-full h-[54px] border-l-8 border-l-primary1 flex items-center pl-5">
+      <h1 class="text-2xl font-bold">작업 요청</h1>
+    </div>
+    <form
+      @submit.prevent="handleSubmit"
+      class="flex flex-col gap-y-6">
+      <RequestTaskDropdown
+        :initValue="category1"
+        :options="REQUEST_TASK_CATEGORIES" />
+      <RequestTaskDropdown
+        :initValue="category1"
+        :options="REQUEST_TASK_CATEGORIES" />
+
+      <div>
+        <label
+          for="title"
+          class="text-xs flex gap-x-1 mb-2">
+          <p class="text-body font-bold">제목</p>
+          <p class="text-red-1">*</p>
+        </label>
+        <input
+          class="w-full h-11 border border-border-1 px-4"
+          type="text"
+          id="title"
+          v-model="title"
+          placeholder="부가 설명을 입력해주세요" />
+      </div>
+      <div>
+        <label
+          for="description"
+          class="text-xs text-body font-bold">
+          부가 설명
+        </label>
+        <textarea
+          class="w-full h-24 mt-2 border border-border-1 px-4 py-2"
+          id="description"
+          v-model="description"
+          placeholder="부가 설명을 입력해주세요">
+        </textarea>
+      </div>
+      <div>
+        <label
+          for="file"
+          class="text-xs text-body font-bold">
+          첨부 파일</label
+        >
+        <input
+          class="hidden"
+          type="file"
+          id="file"
+          @change="handleFileUpload" />
+        <label
+          for="file"
+          class="w-[97px] h-8 mt-2 rounded px-4 py-2 bg-primary1 text-white flex items-center justify-center">
+          파일 선택
+        </label>
+      </div>
+      <div class="flex">
+        <button
+          type="submit"
+          class="w-[188px] h-[52px] rounded text-white bg-primary1 flex items-center justify-center">
+          요청
+        </button>
+        <button
+          type="button"
+          class="w-[188px] h-[52px] border border-disabled rounded text-disabled bg-white flex items-center justify-center"
+          @click="handleCancel">
+          취소
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { REQUEST_TASK_CATEGORIES } from '@/constants/user'
+import { ref } from 'vue'
+import RequestTaskDropdown from './RequestTaskDropdown.vue'
+
+const category1 = ref('')
+const category2 = ref('')
+const title = ref('')
+const description = ref('')
+const file = ref(null as File | null)
+
+const handleFileUpload = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  if (target.files) {
+    file.value = target.files[0]
+  }
+}
+
+const handleCancel = () => {
+  category1.value = ''
+  category2.value = ''
+  title.value = ''
+  description.value = ''
+  file.value = null
+}
+
+const handleSubmit = () => {
+  const formData = new FormData()
+  formData.append('category1', category1.value)
+  formData.append('category2', category2.value)
+  formData.append('title', title.value)
+  formData.append('description', description.value)
+  if (file.value) {
+    formData.append('file', file.value)
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/RequestTaskDropdown.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="flex text-xs gap-x-1 mb-2">
+      <p class="text-body font-bold">2차 카테고리</p>
+      <p class="text-red-1">*</p>
+    </div>
+    <div class="relative flex text-base">
+      <div
+        class="flex w-full h-11 items-center rounded p-4 bg-white border border-border-1 cursor-pointer"
+        @click="toggleDropdown">
+        <p>
+          {{ selectedOption }}
+        </p>
+      </div>
+      <div
+        v-if="dropdownOpen"
+        class="absolute w-full h-40 overflow-y-auto top-[52px] flex flex-col gap-2 p-2 bg-white rounded z-10 shadow">
+        <div
+          v-for="option in REQUEST_TASK_CATEGORIES"
+          :key="option"
+          class="w-full flex items-center h-11 p-2 rounded hover:bg-background-2 cursor-pointer"
+          @click="selectOption(option)">
+          {{ option }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { REQUEST_TASK_CATEGORIES } from '@/constants/user'
+import type { RequestTaskDropdownProps } from '@/types/user'
+import { ref } from 'vue'
+
+const props = defineProps<RequestTaskDropdownProps>()
+
+const dropdownOpen = ref(false)
+const selectedOption = ref(props.initValue)
+// const filter = ref('filter-icon')
+
+const toggleDropdown = () => {
+  dropdownOpen.value = !dropdownOpen.value
+}
+
+const selectOption = (option: string) => {
+  selectedOption.value = option
+  dropdownOpen.value = false
+}
+</script>
+
+<style scoped></style>
