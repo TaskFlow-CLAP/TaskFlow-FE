217557ab01f9fe1455122a59dddaacc4b12cbf08
jiyeon <bjyy0201@gmail.com>
2025-01-22 13:47:22 +0900

    ✨  [feat] : 로그인, 비밀번호 변경 페이지 경로 설정

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,6 +26,7 @@ const router = createRouter({
       component: () => import('../views/PwCheck.vue'),
       meta: { requiresAuth: true }
     },
+
     {
       path: '/icon',
       name: 'icon',
