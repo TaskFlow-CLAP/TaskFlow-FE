f7aa7536385b39c4e1893479998cf56d16028542
jiyeon <bjyy0201@gmail.com>
2025-01-22 15:19:21 +0900

    ✨ [feat] : 비밀번호 재설정 페이지, 비정상적인 경로 차단

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/Login.vue'
 import PwChangeView from '../views/PwChange.vue'
+import PwChangeEmailView from '../views/PwChangeEmail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,31 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
+
     {
       path: '/pw-change',
       name: 'pwChange',
       component: PwChangeView
+    },
+    {
+      path: '/pw-change-email',
+      name: 'pwChangeEmail',
+      component: PwChangeEmailView,
+      meta: { requiresAuth: true }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const isLogined = false // 로그인 상태 서버로부터 받아와야함
+  console.log('isLogined:', isLogined)
+
+  if (to.path === '/pw-change' && !isLogined) {
+    alert('로그인이 필요합니다.')
+    next('/login') // 로그인 페이지로 리다이렉트
+  } else {
+    next() // 접근 허용
+  }
 })
 
 export default router
