0642fa5d15d3d36a790e4f5401bc089fbb60d16b
seorang42 <seorang42@naver.com>
2025-01-24 01:39:31 +0900

    :sparkles: [feat] : ListPagination 컴포넌트 구현

---
+++ src/components/ListPagination.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="w-fit flex items-center gap-2 relative">
+    <div class="flex gap-1 absolute left-0 top-1/2 -translate-x-[calc(100%+8px)] -translate-y-1/2">
+      <button
+        v-if="pageSet[0] !== 1"
+        @click="onPrevSetClick"
+        class="rounded hover:bg-primary2">
+        <CommonIcons :name="prevSetIcon" />
+      </button>
+      <button
+        @click="onPrevClick"
+        class="rounded hover:bg-primary2">
+        <CommonIcons :name="prevIcon" />
+      </button>
+    </div>
+    <button
+      v-for="page in pageSet"
+      :key="page"
+      class="w-6 h-6 flex justify-center items-center rounded text-sm"
+      :class="
+        page === pageNumber ? 'text-white font-bold bg-primary1' : 'text-black hover:bg-primary2'
+      "
+      @click="onNumClick(page)">
+      {{ page }}
+    </button>
+    <div class="flex gap-1 absolute right-0 top-1/2 translate-x-[calc(100%+8px)] -translate-y-1/2">
+      <button
+        @click="onNextClick"
+        class="rounded hover:bg-primary2">
+        <CommonIcons :name="nextIcon" />
+      </button>
+      <button
+        v-if="pageSet[pageSet.length - 1] !== totalPage"
+        @click="onNextSetClick"
+        class="rounded hover:bg-primary2">
+        <CommonIcons :name="nextSetIcon" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { nextIcon, nextSetIcon, prevIcon, prevSetIcon } from '@/constants/iconPath'
+import CommonIcons from './CommonIcons.vue'
+import { computed } from 'vue'
+
+const { pageNumber, totalPage } = defineProps<{ pageNumber: number; totalPage: number }>()
+const emit = defineEmits(['update:pageNumber'])
+
+const pageSet = computed(() => {
+  const set: number[] = []
+  const startPage = Math.floor((pageNumber - 1) / 5) * 5 + 1
+  for (let i = 0; i < 5; i++) {
+    if (startPage + i > totalPage) break
+    set.push(startPage + i)
+  }
+  return set
+})
+
+const onPrevSetClick = () => {
+  const prevSetEnd = Math.floor((pageNumber - 1) / 5) * 5
+  emit('update:pageNumber', prevSetEnd)
+}
+const onPrevClick = () => {
+  const prev = Math.max(pageNumber - 1, 1)
+  emit('update:pageNumber', prev)
+}
+const onNextClick = () => {
+  const next = Math.min(pageNumber + 1, totalPage)
+  emit('update:pageNumber', next)
+}
+const onNextSetClick = () => {
+  const nextSetStart = Math.floor((pageNumber - 1) / 5) * 5 + 6
+  emit('update:pageNumber', nextSetStart)
+}
+const onNumClick = (num: number) => {
+  emit('update:pageNumber', num)
+}
+</script>
+
+<style scoped></style>
--- src/components/my-request/MyRequestList.vue
+++ src/components/my-request/MyRequestList.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="grow overflow-hidden">
-    <MyRequestListBar />
+  <div class="flex flex-col items-center gap-4 grow overflow-hidden">
+    <div class="w-full flex flex-col grow overflow-hidden">
+      <MyRequestListBar />
 
-    <div class="h-full overflow-y-auto">
-      <MyRequestListCard
-        v-for="info in DUMMY_MY_REQUEST_LIST_INFO"
-        :key="info.taskId"
-        :info="info" />
+      <div class="overflow-y-auto">
+        <MyRequestListCard
+          v-for="info in DUMMY_MY_REQUEST_LIST_INFO"
+          :key="info.taskId"
+          :info="info" />
+      </div>
     </div>
+
+    <ListPagination
+      :page-number="params.pageNumber"
+      :total-page="DUMMY_TOTAL_PAGE"
+      @update:page-number="onPageChange" />
   </div>
 </template>
 
@@ -16,6 +23,11 @@ import { DUMMY_MY_REQUEST_LIST_INFO } from '@/constants/dummy'
 import MyRequestListBar from './MyRequestListBar.vue'
 import MyRequestListCard from './MyRequestListCard.vue'
 import { useMyRequestParamsStore } from '@/stores/params'
+import ListPagination from '../ListPagination.vue'
 
 const { params } = useMyRequestParamsStore()
+const DUMMY_TOTAL_PAGE = 18
+const onPageChange = (value: number) => {
+  params.pageNumber = value
+}
 </script>
