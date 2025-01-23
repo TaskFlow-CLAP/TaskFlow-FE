27a45fea3bce09bfa71daf6f51e45407eb4a4169
jiyeon <bjyy0201@gmail.com>
2025-01-23 16:37:07 +0900

    ✨ [feat] : CustomButton, Modal 추가

--- src/views/PwChange.vue
+++ src/views/PwChange.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="max-w-md mx-auto p-10">
+    <Modal
+      :isOpen="isModalOpen"
+      :type="'checkType'"
+      @close="pwChange">
+      <template #header> 비밀번호가 변경 되었습니다 </template>
+      <template #body> 다시 로그인 해주세요 </template>
+    </Modal>
     <div class="py-16">
       <div class="text-4xl font-bold text-center">
         <p class="pb-2">비밀번호</p>
@@ -28,11 +35,12 @@
           required
           class="block w-full px-4 py-4 border border-zinc-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" />
       </div>
-      <button
+
+      <CustomButton
+        color="primaryButton"
         type="submit"
-        class="w-full py-4 px-4 bg-primary1 text-white rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
-        비밀번호 재설정
-      </button>
+        name="비밀번호 재설정"
+        click="handleChange" />
     </form>
     <div class="flex w-full justify-center"></div>
   </div>
@@ -40,25 +48,42 @@
 
 <script lang="ts">
 import { ref } from 'vue'
+import router from '../router/index'
+import Modal from '../components/Modal.vue'
 
 export default {
   name: 'PwChangePage',
+  components: Modal,
   setup() {
     const newPw = ref('')
     const checkPw = ref('')
+    const isModalOpen = ref(false)
+
+    const toggleModal = () => {
+      isModalOpen.value = !isModalOpen.value
+    }
 
     const handleChange: () => void = () => {
       if (newPw.value === checkPw.value) {
-        alert('비밀번호 변경 성공')
+        console.log('asdf')
+        toggleModal()
+        //비밀번호 재설정 API 추가 필요
       } else {
         alert('일치x')
       }
     }
 
+    const pwChange = () => {
+      router.push('/login')
+    }
+
     return {
       newPw,
       checkPw,
-      handleChange
+      handleChange,
+      isModalOpen,
+      toggleModal,
+      pwChange
     }
   }
 }
