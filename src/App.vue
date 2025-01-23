5271ca06e358ca41976c1c23453fcceccbd03288
jiyeon <bjyy0201@gmail.com>
2025-01-23 14:56:02 +0900

    🔥  [remove] :  불필요 요소 제거

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import TopMenu from './components/TopMenu.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <TopMenu />
   <RouterView />
 </template>
 
