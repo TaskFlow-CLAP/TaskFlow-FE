b491d27c56c56259550bb40978d49acda418df03
Minkyu0424 <yng1404@gmail.com>
2025-01-23 10:23:53 +0900

    :sparkles: [feat] : 공용 아이콘 컴포넌트 생성

---
+++ src/components/CommonIcons.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg
+    :width="name?.width"
+    :height="name?.height"
+    :viewBox="`0 0 ${name?.width} ${name?.height}`"
+    :fill="isHovered ? name?.hoverFill : name?.fill"
+    @mouseover="isHovered = true"
+    @mouseleave="isHovered = false"
+    :class="className"
+    @click="onClick">
+    <path
+      v-for="(p, index) in Array.isArray(name?.path) ? name.path : [name?.path]"
+      :key="index"
+      :d="p"
+      v-bind="name?.options" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import type { IconPathTypes } from '@/types/icon'
+import { defineProps, ref, type PropType } from 'vue'
+const isHovered = ref(false)
+const { name, className, onClick } = defineProps({
+  name: Object as PropType<IconPathTypes>,
+  className: {
+    type: String,
+    required: false
+  },
+  onClick: {
+    type: Function as PropType<(event: MouseEvent) => void>,
+    required: false
+  }
+})
+</script>
