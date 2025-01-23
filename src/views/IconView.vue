16ba6f53a371ce6889a2ee7472a5f4bfc162334f
Minkyu0424 <yng1404@gmail.com>
2025-01-23 11:49:27 +0900

    :recycle: [refactor] : 코드 포매팅 적용

--- src/views/IconView.vue
+++ src/views/IconView.vue
@@ -1,75 +1,75 @@
 <template>
   <main class="w-full h-screen">
     <div class="flex items-center mb-8 text-[10px]">
-      <div class='w-20'>
-      <CommonIcons :name="dropdownIcon" />
-      <span>dropdownIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="dropdownIcon" />
+        <span>dropdownIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="hamburgerIcon" />
-      <span>hamburgerIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="hamburgerIcon" />
+        <span>hamburgerIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="notifiacationIcon" />
-      <span>notifiacationIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="notifiacationIcon" />
+        <span>notifiacationIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="bentoIcon" />
-      <span>bentoIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="bentoIcon" />
+        <span>bentoIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="warningIcon" />
-      <span>warningIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="warningIcon" />
+        <span>warningIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="successIcon" />
-      <span>successIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="successIcon" />
+        <span>successIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="failIcon" />
-      <span>failIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="failIcon" />
+        <span>failIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="uploadIcon" />
-      <span>uploadIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="uploadIcon" />
+        <span>uploadIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="deleteIcon" />
-      <span>deleteIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="deleteIcon" />
+        <span>deleteIcon</span>
       </div>
     </div>
     <div class="flex items-center text-[10px]">
-      <div class='w-20'>
-      <CommonIcons :name="smallCheckIcon" />
-      <span>smallCheckIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="smallCheckIcon" />
+        <span>smallCheckIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="plusIcon" />
-      <span>plusIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="plusIcon" />
+        <span>plusIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="calendarIcon" />
-      <span>calendarIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="calendarIcon" />
+        <span>calendarIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="clockIcon" />
-      <span>clockIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="clockIcon" />
+        <span>clockIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="closeIcon" />
-      <span>closeIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="closeIcon" />
+        <span>closeIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="downloadIcon" />
-      <span>downloadIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="downloadIcon" />
+        <span>downloadIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="clipIcon" />
-      <span>clipIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="clipIcon" />
+        <span>clipIcon</span>
       </div>
-      <div class='w-20'>
-      <CommonIcons :name="sendIcon" />
-      <span>sendIcon</span>
+      <div class="w-20">
+        <CommonIcons :name="sendIcon" />
+        <span>sendIcon</span>
       </div>
     </div>
   </main>
