2e1cac828af633645b6a696dc42dbdb900aadd87
jiyeon <bjyy0201@gmail.com>
2025-01-23 16:11:58 +0900

    ✨ [feat] : 네비게이션 가드 내용 수정, 로그인 여부 기준이 아닌 어떤 페이지에서 왔는지를 기준으로 네비게이션 가드 설정

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/Login.vue'
 import PwChangeView from '../views/PwChange.vue'
 import PwChangeEmailView from '../views/PwChangeEmail.vue'
+import PWCheckView from '../views/PwCheck.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,19 +23,27 @@ const router = createRouter({
       name: 'pwChangeEmail',
       component: PwChangeEmailView,
       meta: { requiresAuth: true }
+    },
+    {
+      path: '/pw-check',
+      name: 'PWCheckView',
+      component: PWCheckView,
+      meta: { requiresAuth: true }
     }
   ]
 })
 
 router.beforeEach((to, from, next) => {
-  const isLogined = localStorage.getItem('isLoggedIn') // 로그인 상태 서버로부터 받아와야함, 우선 세션스토리지로 설정
-  console.log('isLogined:', isLogined)
-
-  if (to.path === '/pw-change' && (isLogined == 'false' || isLogined == null)) {
-    alert('로그인이 필요합니다.')
-    next('/login') // 로그인 페이지로 리다이렉트
+  if (to.path === '/pw-change') {
+    if (from.path === '/pw-check' || from.path === '/pw-change-email') {
+      next()
+    } else {
+      alert('비밀번호를 먼저 확인해주세요.')
+      next('/login')
+      return
+    }
   } else {
-    next() // 접근 허용
+    next()
   }
 })
 
