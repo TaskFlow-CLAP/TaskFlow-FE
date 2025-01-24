9c1dc55c5f7c3864074bbd039886f2a7c033e9bc
jiyeon <bjyy0201@gmail.com>
2025-01-24 18:10:23 +0900

    ♻️  [refactor] : 에러 { 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,12 +27,12 @@ const router = createRouter({
       component: () => import('../views/PwCheck.vue'),
       meta: { requiresAuth: true }
     },
-
+    {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
     },
-      {
+    {
       path: '/my-request',
       name: 'MyRequest',
       component: MyRequestView
