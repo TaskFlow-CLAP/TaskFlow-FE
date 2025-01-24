f1a5d2af305a8262209a41682158d88e551270a1
Minkyu0424 <yng1404@gmail.com>
2025-01-24 15:34:20 +0900

    :recycle: [refactor] : 잘못 선언된 상수, 누락된 타입선언

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -5,6 +5,6 @@ export const REQUEST_TASK_CATEGORIES: string[] = [
   'Categroy 4'
 ]
 
-export const TITLE_PLACEHOLDER = '부가 정보를 입력해주세요'
+export const TITLE_PLACEHOLDER = '제목을 입력해주세요'
 
 export const EXPLANATION_PLACEHOLDER = '부가 정보를 입력해주세요'
--- src/types/user.ts
+++ src/types/user.ts
@@ -9,3 +9,13 @@ export interface RequestTaskInputProps {
   modelValue: string
   placeholderText: string
 }
+
+export interface RequestTaskFileInputProps {
+  files: File[] | null
+  removeFile: (index: number) => void
+}
+
+export interface RequestTaskTextAreaProps {
+  modelValue: string
+  placeholderText: string
+}
