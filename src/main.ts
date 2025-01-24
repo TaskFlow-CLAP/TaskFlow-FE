48a0c3a19c5a3976059c19adf7ab609182fe0ec2
jiyeon <bjyy0201@gmail.com>
2025-01-25 00:26:21 +0900

    ♻️  [refactor] : main.ts 불필요 요소 수정

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,10 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
-import Button from './components/Button.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.component('CustomButton', Button)
 app.mount('#app')
