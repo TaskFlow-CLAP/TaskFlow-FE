05a4a5e21ee2ba44f75e11d9fbb5eb30362d077f
Minkyu0424 <yng1404@gmail.com>
2025-01-26 12:07:14 +0900

    :sparkles: [feat] : 연월일, 요일 추가 유틸 함수 생성

--- src/components/task-detail/TaskDetailHistory.vue
+++ src/components/task-detail/TaskDetailHistory.vue
@@ -2,18 +2,18 @@
   <div>
     <p class="task-detail">히스토리</p>
     <TaskDetailHistoryInput :history="history" />
-    <div class="flex flex-col w-full items-center gap-6">
+    <div class="flex flex-col w-full items-center gap-6 mt-8">
       <div
-        class="flex flex-col items-center"
+        class="flex flex-col items-center gap-6"
         v-for="(item, i) in history"
         :key="i">
         <div
           class="flex w-[150px] h-7 items-center justify-center bg-background-1 rounded-xl text-body text-xs font-bold">
-          {{ item.date }}
+          {{ formatDateWithDay(item.date) }}
         </div>
-        <div class="flex">
+        <div class="flex gap-1 text-body text-sm">
           <p>{{ HistoryMessageBefore[item.TaskHistoryType] }}</p>
-          <p>{{ item.details.taskStatus }}</p>
+          <p class="text-primary1">{{ item.details.taskStatus }}</p>
           <p>{{ HistoryMessageAfter[item.TaskHistoryType] }}</p>
         </div>
       </div>
@@ -24,6 +24,7 @@
 <script setup lang="ts">
 import { HistoryMessageAfter, HistoryMessageBefore } from '@/constants/user'
 import type { TaskDetailHistoryProps } from '@/types/user'
+import { formatDateWithDay } from '@/utils/date'
 import TaskDetailHistoryInput from './TaskDetailHistoryInput.vue'
 
 const { history } = defineProps<{ history: TaskDetailHistoryProps[] }>()
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -7,3 +7,14 @@ export const formatDate = (dateString: string) => {
 
   return `${year}.${month}.${day}`
 }
+
+export const formatDateWithDay = (dateString: string) => {
+  const date = new Date(dateString)
+
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const dayOfWeek = date.toLocaleDateString('ko-KR', { weekday: 'long' })
+
+  return `${year}년 ${month}월 ${day}일 ${dayOfWeek}`
+}
