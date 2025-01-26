b2d4e1782de43369b3dd2f382d281a77245e6096
seorang42 <seorang42@naver.com>
2025-01-26 19:41:18 +0900

    :bug: [fix] 회원 관리 페이지 ListBar 레이아웃 오류 수정

--- src/constants/admin.ts
+++ src/constants/admin.ts
@@ -6,8 +6,8 @@ export const MEMBER_MANAGEMENT_LIST_BAR_TAB: ListBarTabProps[] = [
   { content: '부서', width: 80 },
   { content: '직무', width: 80 },
   { content: '이메일' },
-  { content: '역할', width: 120 },
-  { content: '승인 권한', width: 64 },
+  { content: '역할', width: 60 },
+  { content: '승인 권한', width: 60 },
   { content: '가입일', width: 80, sortBy: 'REGISTERED' },
   { content: '처리', width: 180, justifyCenter: true }
 ]
