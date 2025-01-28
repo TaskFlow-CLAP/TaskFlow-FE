add9f6acd015983bc890587838d8cd624521bbdd
Minkyu0424 <yng1404@gmail.com>
2025-01-29 02:42:13 +0900

    :recycle: [refactor] : 요청 승인 모달과의 연결

--- src/components/request-approve/RequestApprove.vue
+++ src/components/request-approve/RequestApprove.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="w-[552px] flex flex-col gap-y-6">
+    <ModalView
+      :isOpen="isModalVisible"
+      :type="'successType'"
+      @close="handleCancel">
+      <template #header> 요청이 승인되었습니다 </template>
+    </ModalView>
     <RequestTaskDropdown
       v-model="approveForm.category1"
       :options="DUMMY_REQUEST_TASK_CATEGORIES"
@@ -57,19 +63,23 @@ import {
   DUMMY_REQUEST_TASK_LABELS
 } from '@/datas/taskdetail'
 import { ref } from 'vue'
+import ModalView from '../ModalView.vue'
 import RequestTaskDropdown from '../request-task/RequestTaskDropdown.vue'
 import DueDateInput from './DueDateInput.vue'
 import ProcessorDropdown from './ProcessorDropdown.vue'
 
+const isModalVisible = ref(false)
 const approveForm = ref(INITIAL_REQUEST_APPROVE_FORM)
 
 const handleCancel = () => {
   approveForm.value = { ...INITIAL_REQUEST_APPROVE_FORM }
+  isModalVisible.value = false
 }
 
 const handleSubmit = () => {
   const formData = new FormData()
   formData.append('approveForm', JSON.stringify(approveForm.value))
   console.log(JSON.parse(formData.get('approveForm') as string))
+  isModalVisible.value = true
 }
 </script>
