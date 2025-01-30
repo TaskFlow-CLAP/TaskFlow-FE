f132826a566fcdf3d41c9463f55f609c068c00b4
seorang42 <seorang42@naver.com>
2025-01-30 18:23:26 +0900

    :sparkles: [feat] : 통계 페이지 UI 구현

--- src/types/manager.ts
+++ src/types/manager.ts
@@ -69,36 +69,4 @@ export interface TeamBoardCardProps {
   tasks: TaskCardProps[]
 }
 
-export interface RequestApproveFormData {
-  category1: string
-  category2: string
-  processor: string
-  labeling: string
-  dueDate: string
-  dueTime: string
-}
-
-export interface MyTaskDetailDatas {
-  taskId: number
-  taskCode: string
-  requestedAt: string
-  finishedAt: string
-  taskStatus: Status
-  requesterNickName: string
-  requesterImageUrl: string
-  processorNickName: string
-  processorImageUrl: string
-  mainCategoryName: string
-  categoryName: string
-  title: string
-  description: string
-  dueDate: string
-  labelName: string
-  attachmentResponses: AttachmentResponse[]
-}
-
-export interface TaskDetailTopBarProps {
-  isManager: boolean
-  isApproved: boolean
-  closeTaskDetail: () => void
-}
+export type PeriodType = 'DAY' | 'WEEK' | 'MONTH'
