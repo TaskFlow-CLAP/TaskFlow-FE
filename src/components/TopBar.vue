3917389b2866552d5b4279748ca665917a81c23b
jiyeon <bjyy0201@gmail.com>
2025-01-30 14:54:55 +0900

    💄 [design] : TopBar UI 구현 및 파일명 변경

---
+++ src/components/TopBar.vue
@@ -0,0 +1,45 @@
+//Top.vue
+<template>
+  <div
+    class="flex justify-center w-full bg-white text-black py-3 px-[120px] border border-border-1">
+    <div class="container flex w-full justify-between items-center">
+      <div class="flex justify-center items-center gap-2 h-full">
+        <button @click="onChangeSide">
+          <CommonIcons
+            v-show="isLogined"
+            :name="hamburgerIcon" />
+        </button>
+        <img src="../../public/MainLogo.svg" />
+      </div>
+      <div class="w-[60%] h-full"></div>
+      <div
+        v-show="isLogined"
+        class="w-[10%] h-full flex items-center gap-6">
+        <CommonIcons :name="notifiacationIcon" />
+        <div class="rounded-[50%] bg-zinc-100 p-5" />
+      </div>
+    </div>
+  </div>
+  <SideBar
+    v-if="isSideOpen"
+    @close="onCloseSide" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import CommonIcons from './common/CommonIcons.vue'
+import SideBar from './SideBar.vue'
+import { hamburgerIcon, notifiacationIcon } from '../constants/iconPath'
+
+// 로그인 정보 필요
+const isSideOpen = ref(false)
+const isLogined = ref(true)
+
+const onChangeSide = () => {
+  isSideOpen.value = true
+}
+
+const onCloseSide = () => {
+  isSideOpen.value = false
+}
+</script>
