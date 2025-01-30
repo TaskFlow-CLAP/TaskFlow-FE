45fabd273be1f45f1558f0f87656c575bf7c1576
Minkyu0424 <yng1404@gmail.com>
2025-01-31 01:59:52 +0900

    :sparkles: [feat] : 구분 목록, 추가, 삭제 UI

--- src/views/TaskManagementView.vue
+++ src/views/TaskManagementView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="list-view">
+  <div class="task-management-view">
     <div class="form-view-title">
       <h1 class="text-2xl font-bold">작업 관리</h1>
     </div>
@@ -7,10 +7,11 @@
     <CategoryManagement />
     <div class="w-full h-[1px] bg-border-2"></div>
     <p class="text-xl">구분</p>
-    <RequestTask />
+    <DivisionManagement />
   </div>
 </template>
 
 <script setup lang="ts">
 import CategoryManagement from '@/components/task-management/CategoryManagement.vue'
+import DivisionManagement from '@/components/task-management/DivisionManagement.vue'
 </script>
