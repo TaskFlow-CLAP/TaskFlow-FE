8956434d8ede866f207ada23ec14b2d355483519
jiyeon <bjyy0201@gmail.com>
2025-01-31 20:03:28 +0900

    ✨  [feat] : 카테고리 폼 타입 설정

--- src/types/admin.ts
+++ src/types/admin.ts
@@ -21,6 +21,18 @@ export interface LogsListData {
   status: number
   result?: string
 }
+
+export interface CategoryFirstData {
+  name: string
+  code: string
+}
+
+export interface CategorySecondData {
+  name: string
+  firstCategory: string
+  code: string
+}
+
 export interface UserRegistrationProps {
   name: string
   email: string
@@ -31,6 +43,47 @@ export interface UserRegistrationProps {
   departmentRole: string
 }
 
-export type Role = '관리자' | '사용자' | '담당자'
+export type RoleTypes = '관리자' | '사용자' | '담당자'
 
-export type RoleTypes = 'ROLE_ADMIN' | 'ROLE_USER' | 'ROLE_MANAGER'
+export type RoleTypesEnum = 'ROLE_ADMIN' | 'ROLE_USER' | 'ROLE_MANAGER'
+
+export interface CategoryLineProps {
+  name?: string
+  code?: string
+  mainCategoryId?: string
+}
+
+export interface mainCategoryTypes {
+  id: number
+  name: string
+  code: string
+}
+
+export interface subCategoryTypes {
+  id: number
+  mainCategoryId: number
+  name: string
+  code: string
+}
+
+export interface categoriesTypes {
+  id: number
+  name: string
+  code: string
+  subCategory: subCategoryTypes[]
+}
+
+export interface CategoryAllData {
+  categories: categoriesTypes[]
+}
+
+export interface DivisionDataTypes {
+  divisionId: number
+  divisionName: string
+  divisionColor: string
+}
+
+export interface NewDevisonTypes {
+  divisionName: string
+  divisionColor: string
+}
