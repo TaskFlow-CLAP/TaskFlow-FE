e653ac89e1ec59ac173121fe9e4f7a93857b1253
jiyeon <bjyy0201@gmail.com>
2025-01-31 20:03:43 +0900

    💄 [design] : 카테고리 생성 페이지 구현

---
+++ src/views/CategorySecondAdd.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="form-view-container">
+    <div class="form-view-title">
+      <h1 class="text-2xl font-bold">2차 카테고리 생성</h1>
+    </div>
+    <CategoryAdd categoryStep="2" />
+  </div>
+  <div class="w-full bg-body"></div>
+</template>
+
+<script setup lang="ts">
+import CategoryAdd from '../components/task-management/CategoryAdd.vue'
+</script>
+
+<style scoped></style>
