ebae6234e857d57c5cbf29b73fab29b09f6c7716
Minkyu0424 <yng1404@gmail.com>
2025-02-02 03:11:02 +0900

    :sparkles: [feat] : 요청 생성 api 연결

--- src/types/common.ts
+++ src/types/common.ts
@@ -128,6 +128,7 @@ export interface CategoryDropdownProps {
   placeholderText: string
   options: MainCategoryTypes[] | SubCategoryTypes[]
   labelName: string
-  modelValue: string
+  modelValue: MainCategoryTypes | SubCategoryTypes | null
   isLabel?: boolean
+  isDisabled?: boolean
 }
