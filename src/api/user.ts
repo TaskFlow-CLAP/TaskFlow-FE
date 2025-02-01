0c1acb99821d2a7b6c2c7efece5a47f4481a3381
Minkyu0424 <yng1404@gmail.com>
2025-02-01 17:55:52 +0900

    :sparkles: [feat] : 폼데이터 요청 api 형식 분리

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,13 +4,3 @@ export const postTaskRequest = async (formdata: FormData) => {
   const response = await formDataAxiosInstance.post('/api/tasks', formdata)
   return response.data
 }
-
-export const getTaskDetailUser = async (id: number) => {
-  const response = await formDataAxiosInstance.get(`/api/tasks/${id}/requests/details`)
-  return response.data
-}
-
-export const getTaskDetailManager = async (id: number) => {
-  const response = await formDataAxiosInstance.get(`/api/tasks/${id}/details`)
-  return response.data
-}
