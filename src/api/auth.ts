6ccd304b98e641525f98f4b90da33941a6be6057
jiyeon <bjyy0201@gmail.com>
2025-02-02 23:51:56 +0900

    ♻️ [refactor] : 불필요한 예외처리 제거

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -7,41 +7,31 @@ export const postLogin = async (nickname: string, password: string) => {
     password: password
   }
   const sessionIdValue = '123'
-  try {
-    const response = await axiosInstance.post('/api/auths/login', body, {
-      headers: {
-        sessionId: sessionIdValue
-      }
-    })
 
-    Cookies.set('accessToken', response.data.accessToken, {
-      path: '/',
-      sameSite: 'strict'
-    })
+  const response = await axiosInstance.post('/api/auths/login', body, {
+    headers: {
+      sessionId: sessionIdValue
+    }
+  })
 
-    return response.data
-  } catch (error) {
-    console.error('로그인 오류:', error)
-    throw error
-  }
+  Cookies.set('accessToken', response.data.accessToken, {
+    path: '/',
+    sameSite: 'strict'
+  })
+
+  return response.data
 }
 
 export const patchPassword = async (password: string) => {
   const accessToken = Cookies.get('accessToken')
 
-  if (!accessToken) {
-    console.error('Access token is missing')
-    return
-  }
+  if (!accessToken) return
 
-  try {
-    const response = await axiosInstance.patch('/api/members/password', password, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log('api error:', error)
-  }
+  const response = await axiosInstance.patch('/api/members/password', password, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+
+  return response.data
 }
