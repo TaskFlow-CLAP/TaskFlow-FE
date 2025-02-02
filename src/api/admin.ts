12b07a8a49af43a7483bffb1e7abdec86a9a9d57
Minkyu0424 <yng1404@gmail.com>
2025-02-02 14:45:41 +0900

    :recycle: [refactor] : 모달 및 상태관리 함수 하나로 변경

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -4,3 +4,8 @@ export const getLabelsAdmin = async () => {
   const response = await axiosInstance.get('/api/managements/labels')
   return response.data
 }
+
+export const deleteLabelAdmin = async (id: string) => {
+  const response = await axiosInstance.delete(`/api/managements/labels/${id}`)
+  return response.data
+}
