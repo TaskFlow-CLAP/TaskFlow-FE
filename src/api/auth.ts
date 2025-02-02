9f952ae38e859bc377688ffdafe1bc436e1ab863
jiyeon <bjyy0201@gmail.com>
2025-02-02 20:56:59 +0900

    ♻️ [refactor] : axios 인스턴스 이슈 반영

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,4 @@
-import axiosInstance from '@/utils/axios'
+import { axiosInstance } from '@/utils/axios'
 import Cookies from 'js-cookie'
 
 export const postLogin = async (nickname: string, password: string) => {
