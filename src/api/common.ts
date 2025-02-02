2b4694a470d90d2a914f6d290b17bcbf10142879
Minkyu0424 <yng1404@gmail.com>
2025-02-03 01:41:34 +0900

    :sparkles: [feat] : 작업 관리 카테고리 전체 조회 연결

--- src/api/common.ts
+++ src/api/common.ts
@@ -9,3 +9,8 @@ export const getSubCategory = async () => {
   const response = await axiosInstance.get('/api/sub-category')
   return response.data
 }
+
+export const getAllCategory = async () => {
+  const response = await axiosInstance.get('/api/category')
+  return response.data
+}
