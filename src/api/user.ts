e5f97026a799094a4f7b1064a85a8067db0e72bd
Minkyu0424 <yng1404@gmail.com>
2025-02-03 16:51:31 +0900

    :recycle: [refactor] : 담당자용 api get 연결 요청승인 시 승인 경로로 이동

--- src/api/user.ts
+++ src/api/user.ts
@@ -9,3 +9,8 @@ export const getTaskDetailUser = async (id: number) => {
   const response = await formDataAxiosInstance.get(`/api/tasks/${id}/requests/details`)
   return response.data
 }
+
+export const getTaskDetailManager = async (id: number) => {
+  const response = await formDataAxiosInstance.get(`/api/tasks/${id}/details`)
+  return response.data
+}
