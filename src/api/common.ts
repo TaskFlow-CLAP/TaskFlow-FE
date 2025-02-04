d40fc80a6c048ef1262d0b0fc90feec9dd1d236c
jiyeon <bjyy0201@gmail.com>
2025-02-04 15:18:27 +0900

    ✨  [feat] : 알림목록 조회 API 추가

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,5 +1,10 @@
 import { axiosInstance } from '../utils/axios'
 
+export const getNotification = async (pageNum: number, sizeNum: number) => {
+  const response = await axiosInstance.get(`/api/notifications?page=${pageNum}&size=${sizeNum}`)
+  return response.data
+}
+
 export const getMainCategory = async () => {
   const response = await axiosInstance.get('/api/main-category')
   return response.data
