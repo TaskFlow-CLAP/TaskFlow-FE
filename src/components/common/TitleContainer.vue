7f61b6e30c3cad5fe32a41a59450ca95ea590949
seorang42 <seorang42@naver.com>
2025-02-04 18:33:51 +0900

    :sparkles: [feat] : 404 페이지 구현 및 TitleContainer 컴포넌트 생성

---
+++ src/components/common/TitleContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col gap-2 items-center">
+    <span class="text-4xl font-bold text-black whitespace-pre-wrap text-center leading-tight">{{
+      title
+    }}</span>
+    <span class="font-bold whitespace-pre-wrap text-body text-center">{{ content }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ title: string; content?: string }>()
+</script>
