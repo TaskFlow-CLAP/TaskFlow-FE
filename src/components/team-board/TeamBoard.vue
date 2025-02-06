7518848f236b91d5ce2191225b0d5f01b129acab
seorang42 <seorang42@naver.com>
2025-02-06 16:11:46 +0900

    :sparkles: [feat] : 팀 작업 현황 API 연결

--- src/components/team-board/TeamBoard.vue
+++ src/components/team-board/TeamBoard.vue
@@ -5,42 +5,24 @@
         :team-summary="teamSummary"
         :team-data="teamData" />
       <TeamBoardCard
-        v-for="member in DUMMY_TEAM_MEMBERS_LIST"
-        :key="member.name"
-        :name="member.name"
-        :department="member.department"
-        :task-status-summary="member.taskStatusSummary"
-        :tasks="member.tasks" />
+        v-for="member in data?.members"
+        :key="member.processorId"
+        :info="member" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { DUMMY_TEAM_MEMBERS_LIST } from '@/datas/dummy'
 import TeamBoardCard from './TeamBoardCard.vue'
 import CurrentTaskRatio from './CurrentTaskRatio.vue'
 import { axiosInstance } from '@/utils/axios'
 import { useTeamBoardParamsStore } from '@/stores/params'
 import { useQuery } from '@tanstack/vue-query'
 import { useParseParams } from '../hooks/useParseParams'
+import type { TeamBoardResponse } from '@/types/manager'
+import { computed } from 'vue'
 
 const { params } = useTeamBoardParamsStore()
-
-const getSummaryCount = (key: 'inProgress' | 'pendingCompletion' | 'totalTasks') => {
-  let count = 0
-  DUMMY_TEAM_MEMBERS_LIST.forEach(el => (count += el.taskStatusSummary[key]))
-  return count
-}
-const teamSummary = {
-  inProgress: getSummaryCount('inProgress'),
-  pendingCompletion: getSummaryCount('pendingCompletion'),
-  totalTasks: getSummaryCount('totalTasks')
-}
-
-const teamData = DUMMY_TEAM_MEMBERS_LIST.map(el => ({
-  name: el.name,
-  tasks: el.taskStatusSummary.totalTasks
-}))
 
 const fetchTeamStatus = async () => {
   const { parseBoardParams } = useParseParams()
@@ -48,8 +30,25 @@ const fetchTeamStatus = async () => {
   const response = await axiosInstance.get('/api/team-status/filter', { params: parsedParams })
   return response.data
 }
-const { data } = useQuery({
+const { data } = useQuery<TeamBoardResponse>({
   queryKey: ['teamStatus', params],
   queryFn: fetchTeamStatus
+})
+
+const teamSummary = computed(() => {
+  return {
+    inProgress: data.value?.totalInProgressTaskCount || 0,
+    inReviewing: data.value?.totalInReviewingTaskCount || 0,
+    totalTasks: data.value?.totalTaskCount || 0
+  }
+})
+
+const teamData = computed(() => {
+  return (
+    data.value?.members.map(el => ({
+      name: el.nickname,
+      tasks: el.totalTaskCount
+    })) || []
+  )
 })
 </script>
--- src/types/manager.ts
+++ src/types/manager.ts
@@ -64,10 +64,14 @@ export interface MyTaskListData {
 }
 
 export interface TeamBoardCardProps {
-  name: string
   department: string
-  taskStatusSummary: { inProgress: number; pendingCompletion: number; totalTasks: number }
+  imageUrl?: string
+  inProgressTaskCount: number
+  inReviewingTaskCount: number
+  nickname: string
+  processorId: number
   tasks: TaskCardProps[]
+  totalTaskCount: number
 }
 
 export type PeriodType = 'DAY' | 'WEEK' | 'MONTH'
@@ -164,4 +168,11 @@ export interface ManagerTypes {
   nickname: string
   imageUrl: string
   remainingTasks: number
+}
+
+export interface TeamBoardResponse {
+  members: TeamBoardCardProps[]
+  totalInProgressTaskCount: number
+  totalInReviewingTaskCount: number
+  totalTaskCount: number
 }
