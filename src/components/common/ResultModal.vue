10d43c9af115d051c160b86a00e65a3cfbd8f1a9
seorang42 <seorang42@naver.com>
2025-02-06 19:16:54 +0900

    :sparkles: [feat] : 회원 초대 이메일 발송 API 연결

---
+++ src/components/common/ResultModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <ModalView
+    :type="type"
+    :is-open="isOpen"
+    @close="emit('close')">
+    <template #header>{{ message }}</template>
+  </ModalView>
+</template>
+
+<script setup lang="ts">
+import ModalView from '../ModalView.vue'
+
+const { type, isOpen, message } = defineProps<{
+  type: string
+  isOpen: boolean
+  message: string
+}>()
+const emit = defineEmits(['close'])
+</script>
