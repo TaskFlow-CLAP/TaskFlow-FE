df39b1bfdfba84807b20f2ae8203cece729c7c5d
jiyeon <bjyy0201@gmail.com>
2025-02-07 20:08:28 +0900

    ✨  [feat] : 이메일 비밀번호 발급 에러 처리

--- src/views/PwChangeEmail.vue
+++ src/views/PwChangeEmail.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="max-w-400">
     <ModalView
-      :isOpen="isModalOpen"
+      :isOpen="isModalVisible"
       type="successType"
       @close="closeModal">
       <template #header> 인증 번호가 전송되었습니다 </template>
       <template #body> 이메일을 확인해주세요 </template>
+    </ModalView>
+    <ModalView
+      :isOpen="isFailModalVisible"
+      type="failType"
+      @close="closeFailModal">
+      <template #header> {{ messageHeader }} </template>
+      <template #body> {{ messageBody }}</template>
     </ModalView>
     <div class="py-16">
       <TitleContainer
@@ -50,17 +57,40 @@ import ModalView from '../components/ModalView.vue'
 import TitleContainer from '@/components/common/TitleContainer.vue'
 import { postPasswordEmailSend } from '@/api/auth'
 
+const messageHeader = ref('')
+const messageBody = ref('')
+const isModalVisible = ref(false)
+const isFailModalVisible = ref(false)
+
 const name = ref('')
 const email = ref('')
-const isModalOpen = ref(false)
 
 const closeModal = () => {
-  isModalOpen.value = !isModalOpen.value
+  isModalVisible.value = !isModalVisible.value
   router.push('/login')
 }
+const closeFailModal = () => {
+  isFailModalVisible.value = !isFailModalVisible.value
+}
 
-const handleCheck = () => {
-  postPasswordEmailSend(name.value, email.value)
-  isModalOpen.value = !isModalOpen.value
+const handleCheck = async () => {
+  try {
+    await postPasswordEmailSend(name.value, email.value)
+    isModalVisible.value = !isModalVisible.value
+  } catch (error) {
+    switch (error?.response?.status) {
+      case 404:
+        isFailModalVisible.value = !isFailModalVisible.value
+        messageHeader.value = '일치하는 정보가 없습니다'
+        messageBody.value = '이메일과 이름을 다시 확인해 주세요'
+        break
+
+      case 500:
+        isFailModalVisible.value = !isFailModalVisible.value
+        messageHeader.value = '서버에 문제가 발생했습니다'
+        messageBody.value = '잠시후 다시 이용해주세요'
+        break
+    }
+  }
 }
 </script>
