0e7712629a50efd64d6716b69cb95f5f3e962818
Minkyu0424 <yng1404@gmail.com>
2025-02-07 15:36:51 +0900

    :recycle: [refactor] : 파일 업로드 제한

--- src/components/request-task/RequestTaskFileInput.vue
+++ src/components/request-task/RequestTaskFileInput.vue
@@ -22,13 +22,21 @@
         <p>파일 선택</p>
       </label>
     </div>
+    <ModalView
+      :is-open="isModalVisible"
+      type="failType"
+      @close="handleModal">
+      <template #header>파일추가를 실패했습니다</template>
+      <template #body>최대 5개, 각 5mb까지 가능합니다.</template>
+    </ModalView>
   </div>
 </template>
 
 <script lang="ts" setup>
 import CommonIcons from '@/components/common/CommonIcons.vue'
 import { uploadIcon } from '@/constants/iconPath'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import ModalView from '../ModalView.vue'
 import RequestTaskFileInputAfter from './RequestTaskFileInputAfter.vue'
 
 const { modelValue } = defineProps<{
@@ -37,12 +45,26 @@ const { modelValue } = defineProps<{
 const emit = defineEmits(['update:modelValue'])
 
 const hasFiles = computed(() => modelValue && modelValue.length > 0)
+const isModalVisible = ref(false)
+
+const handleModal = () => {
+  isModalVisible.value = !isModalVisible.value
+}
 
 const handleFileUpload = (event: Event) => {
   const target = event.target as HTMLInputElement
   if (target.files && target.files.length > 0) {
-    const newFiles = Array.from(target.files)
-    const updatedFiles = modelValue ? [...modelValue, ...newFiles] : newFiles
+    const newFiles = Array.from(target.files).filter(file => file.size <= 5 * 1024 * 1024)
+    if (newFiles.length !== target.files.length) {
+      handleModal()
+      return
+    }
+    const updatedFiles = modelValue ? [...modelValue, ...target.files] : target.files
+    if (updatedFiles.length > 5) {
+      handleModal()
+      return
+    }
+
     emit('update:modelValue', updatedFiles)
   }
 }
