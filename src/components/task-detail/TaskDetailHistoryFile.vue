17e26aa6e4fa20b89af5c015fd27c25623ce8329
Minkyu0424 <yng1404@gmail.com>
2025-02-10 15:43:53 +0900

    :recycle: [refactor] : 사진누락되는 상황 + 히스토리 댓글삭제

--- src/components/task-detail/TaskDetailHistoryFile.vue
+++ src/components/task-detail/TaskDetailHistoryFile.vue
@@ -1,9 +1,9 @@
 <template>
   <div :class="['flex w-full', isProcessor ? 'justify-end' : 'justify-start']">
     <div :class="['w-10 h-10 rounded-full pt-1.5', isProcessor ? 'order-3' : 'order-1']">
-      <img
-        :src="history.details.commentFileDetails?.profileImageUrl || '/images/mockProfile.jpg'"
-        class="rounded-full" />
+      <ImageContainer
+        :url="history.details.commentDetails?.profileImageUrl"
+        :size="40" />
     </div>
     <div :class="['flex flex-col gap-2 px-4 order-2', isProcessor ? 'items-end' : 'items-start']">
       <p>{{ history.details.commentFileDetails?.nickName }}</p>
@@ -28,18 +28,18 @@
     </div>
     <div
       :class="[
-        'flex h-full items-end text-xs font-bold text-body',
+        'flex flex-col justify-end h-full gap-1 text-xs font-bold text-body',
         isProcessor ? 'ml-4 order-1' : 'ml-2 order-3'
       ]">
       <div
-        v-if="history.details.commentDetails?.nickName === info.nickname"
+        v-if="history.details.commentFileDetails?.nickName === info.nickname"
         class="relative cursor-pointer">
         <CommonIcons
           :name="menuDotIcon"
           @click="clickMenuDot" />
         <div
           v-if="isClicked"
-          @click="deleteCommentText"
+          @click="deleteCommentFile"
           :class="[
             'absolute shadow-custom bottom-0 w-20 h-7 flex items-center justify-center text-xs text-red-1 bg-white hover:bg-background-1',
             isProcessor ? 'right-6' : 'left-6'
@@ -47,8 +47,10 @@
           삭제
         </div>
       </div>
-      {{ history.details.commentFileDetails?.isModified ? '(수정됨)' : '' }}
-      {{ formatTodayOrNot(history.date, history.time) }}
+      <div>
+        {{ history.details.commentFileDetails?.isModified ? '(수정됨)' : '' }}
+        {{ formatTodayOrNot(history.date, history.time) }}
+      </div>
     </div>
   </div>
 </template>
@@ -63,8 +65,10 @@ import { useQueryClient } from '@tanstack/vue-query'
 import { storeToRefs } from 'pinia'
 import { computed, defineProps, ref } from 'vue'
 import CommonIcons from '../common/CommonIcons.vue'
+import ImageContainer from '../common/ImageContainer.vue'
 
 const { history, requestorName, taskId } = defineProps<TaskDetailHistoryChatProps>()
+console.log(history, ' 히스토리')
 
 const memberStore = useMemberStore()
 const { info } = storeToRefs(memberStore)
@@ -74,11 +78,13 @@ const clickMenuDot = async () => {
   isClicked.value = !isClicked.value
 }
 
-const deleteCommentText = async () => {
+const deleteCommentFile = async () => {
   isClicked.value = !isClicked.value
-  await deleteComment(history.historyId)
+  if (history.details.commentFileDetails?.commentId !== undefined) {
+    await deleteComment(history.details.commentFileDetails?.commentId)
+  }
   queryClient.invalidateQueries({ queryKey: ['historyData', taskId] })
 }
 
-const isProcessor = computed(() => history.details.commentFileDetails?.nickName !== requestorName)
+const isProcessor = computed(() => info.value.nickname !== requestorName)
 </script>
