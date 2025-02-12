48cf6c4fba68b79a53d78d5a2eef945bda69692c
Minkyu0424 <yng1404@gmail.com>
2025-02-13 02:54:25 +0900

    :recycle: [fix] : 작업수정 및 재요청에서 허용 길이제한 보이게 수정

--- src/components/request-task/ReRequestTask.vue
+++ src/components/request-task/ReRequestTask.vue
@@ -18,11 +18,13 @@
       v-model="title"
       :placeholderText="'제목을 입력해주세요'"
       :label-name="'제목'"
+      :limit-length="30"
       :is-invalidate="isInvalidate" />
     <RequestTaskTextArea
       v-model="description"
       :is-invalidate="isInvalidate"
-      :placeholderText="'부가 정보를 입력해주세요'" />
+      :placeholderText="'부가 정보를 입력해주세요'"
+      :limit-length="200" />
     <RequestTaskFileInput v-model="file" />
     <FormButtonContainer
       :handleCancel="handleCancel"
