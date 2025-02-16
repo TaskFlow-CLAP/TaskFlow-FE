3cf09fbd7f4360a3493429522de3db3c5faee034
Minkyu0424 <yng1404@gmail.com>
2025-02-16 22:28:34 +0900

    :recycle: [refactor] : conflict resolved

---
+++ src/components/user-manage/DisabledInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative w-full">
+    <div class="text-xs flex gap-x-1 mb-2 text-body font-semibold">
+      {{ labelName }}
+      <p class="text-red-1">*</p>
+    </div>
+    <div
+      class="w-full h-11 border px-4 border-border-1 rounded text-gray-1 bg-background-2 flex items-center">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  labelName: string
+  value: string
+}>()
+</script>
