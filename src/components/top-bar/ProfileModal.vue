14678e7572197a50952ece97ba62f72dd410334d
seorang42 <seorang42@naver.com>
2025-02-17 23:28:45 +0900

    :recycle: [refactor] 피그마와 UI 일치화 작업 및 최종 수정사항 반영

--- src/components/top-bar/ProfileModal.vue
+++ src/components/top-bar/ProfileModal.vue
@@ -2,15 +2,15 @@
   <div
     v-if="isOpen"
     @click.stop
-    class="absolute w-60 bg-white right-6 top-[calc(100%+16px)] rounded-lg shadow-custom overflow-hidden">
+    class="absolute w-52 bg-white right-6 top-[calc(100%+16px)] rounded-lg shadow-custom overflow-hidden">
     <div
-      class="flex items-center justify-center relative px-6 py-8 border-b bg-primary2 border-border-2">
+      class="flex items-center justify-center relative px-6 pt-8 pb-6 border-b bg-primary2 border-border-2">
       <div class="flex flex-col items-center justify-center gap-6">
         <ImageContainer
           :url="info.profileImageUrl"
           :size="96" />
         <div>
-          <div class="flex flex-col justify-center items-center w-[172px]">
+          <div class="flex flex-col justify-center items-center w-full">
             <p class="text-xs text-body font-semibold">{{ name }}</p>
 
             <p>{{ nickname }}</p>
@@ -21,13 +21,13 @@
     <button
       @click="handleEdit"
       type="button"
-      class="flex w-full py-4 px-6 text-sm font-semibold border-b border-border-2 hover:bg-background-2">
+      class="flex w-full py-3.5 px-6 text-sm border-b border-border-2 hover:bg-background-2">
       내 정보 수정
     </button>
     <button
       @click="handleLogout"
       type="button"
-      class="flex w-full py-4 px-6 text-sm text-red-1 font-semibold hover:bg-background-2">
+      class="flex w-full py-3.5 px-6 text-sm text-red-1 font-semibold hover:bg-background-2">
       로그아웃
     </button>
   </div>
